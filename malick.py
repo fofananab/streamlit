6d591ed4b20a6e9e5dec510e0010115bbff1df13
fofananab <165816730+fofananab@users.noreply.github.com>
2024-07-24 16:15:23 +0000

    Create malick.py

---
+++ malick.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+data=pd.read_csv('Expresso_churn_dataset (2).csv')
+st.title("assata")
+data.head()
+data.info()
+data.describe()
+
+data.duplicated
+data.drop_duplicates(inplace=True)
+
+data.isnull().sum()
+data.dropna(inplace=True)
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+data['TENURE']=le.fit_transform(data['TENURE'])
+data['REGION']=le.fit_transform(data['REGION'])
+data['TOP_PACK']=le.fit_transform(data['TOP_PACK'])
